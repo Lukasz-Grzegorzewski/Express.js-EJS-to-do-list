fe0657d81cef32308f8dd4604de456ed026219cb
Lukasz Grzegorzewski <grzegorzewski.luk@gmail.com>
2022-11-01 19:02:36 +0100

    finish to-do-list app

---
+++ date.js
@@ -0,0 +1,21 @@
+exports.getDate = function() {    
+
+    const today = new Date();
+
+    const options = {
+        weekday: 'long',
+        month: 'long', 
+        day: 'numeric'
+    }
+    return today.toLocaleDateString("fr-FR", options);
+}
+
+exports.getDay = function() {    
+
+    const today = new Date();
+
+    const options = {
+        weekday: 'long'
+    }
+    return today.toLocaleDateString("fr-FR", options);
+}
